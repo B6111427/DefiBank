5c7dcfc03c82cb0aeb1a14ed8e0aaa27600047ec
Suphachai Phetthamrong <suphachai.phetthamrong@dkatalis.com>
2022-07-15 00:21:53 +0700

    Add hardhat script

--- defibank/src/Components/Header.tsx
+++ defibank/src/Components/Header.tsx
@@ -7,14 +7,14 @@ import {
   Switch,
   Text,
   useTheme,
-} from "@nextui-org/react";
-import { useTheme as useNextTheme } from "next-themes";
-import { Link } from "react-router-dom";
-import { MoonIcon, SunIcon } from "../Icons";
+} from '@nextui-org/react'
+import { useTheme as useNextTheme } from 'next-themes'
+import { Link } from 'react-router-dom'
+import { MoonIcon, SunIcon } from '../Icons'
 
 export default function Header() {
-  const { setTheme } = useNextTheme();
-  const { isDark } = useTheme();
+  const { setTheme } = useNextTheme()
+  const { isDark } = useTheme()
 
   return (
     <Container xl>
@@ -24,7 +24,7 @@ export default function Header() {
             h2
             css={{
               textGradient:
-                "90deg, $purple600 30%, $gray600 35%, $blue600 50%, $blue600 70%, $yellow600 95%",
+                '90deg, $purple600 30%, $gray600 35%, $blue600 50%, $blue600 70%, $yellow600 95%',
             }}
             weight="bold"
           >
@@ -32,9 +32,9 @@ export default function Header() {
           </Text>
 
           <nav>
-            {" "}
-            <Link to="/">Home</Link> | <Link to="create">Create Account</Link> |{" "}
-            <Link to="transfer">Tranfer</Link>{" "}
+            {' '}
+            <Link to="/">Home</Link> | <Link to="create">Create Account</Link> |{' '}
+            <Link to="transfer">Tranfer</Link>{' '}
           </nav>
         </Grid>
         <Grid xs={4} justify="space-between">
@@ -43,7 +43,7 @@ export default function Header() {
             size="lg"
             iconOn={<SunIcon filled />}
             iconOff={<MoonIcon filled />}
-            onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+            onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
           />
           <Button auto color="gradient" rounded bordered>
             CONNECT WALLET
@@ -51,5 +51,5 @@ export default function Header() {
         </Grid>
       </Grid.Container>
     </Container>
-  );
+  )
 }

d5e973d3330147ec5fd89a62450864634f72ccb4
Suphachai Phetthamrong <suphachai.phetthamrong@dkatalis.com>
2022-07-14 11:31:30 +0700

    Add header to page

--- defibank/src/App.tsx
+++ defibank/src/App.tsx
@@ -8,11 +8,13 @@ import {
 } from "@nextui-org/react";
 import { useTheme as useNextTheme } from "next-themes";
 import { Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
 import Home from "./PageViews/Home";
 
 function App() {
   return (
     <Container xl css={{ background: "$background" }}>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
--- defibank/src/Components/Header.tsx
+++ defibank/src/Components/Header.tsx
@@ -1,4 +1,11 @@
-import { Card, Container, Grid, Switch, Text, useTheme } from "@nextui-org/react";
+import {
+  Card,
+  Container,
+  Grid,
+  Switch,
+  Text,
+  useTheme,
+} from "@nextui-org/react";
 import { useTheme as useNextTheme } from "next-themes";
 
 export default function Header() {
@@ -24,26 +31,36 @@ export default function Header() {
     <Container xl>
       <Grid.Container gap={2} justify="center">
         <Grid xs={9}>
-          <MockItem text="1 of 3" />
+          <Card css={{ h: "$24", $$cardColor: "$colors$primary" }}>
+            <Card.Body>
+              <Text
+                h1
+                size={60}
+                css={{
+                  textGradient: "90deg, $purple600 10%, $blue700 50%",
+                }}
+                weight="bold"
+              >
+                10XBank
+              </Text>
+            </Card.Body>
+          </Card>
         </Grid>
         <Grid xs={3}>
-          <MockItem text="3 of 3" />
+          <Card css={{ h: "$24", $$cardColor: "$colors$primary" }}>
+            <Card.Body>
+              <Text h6 size={15} color="white" css={{ mt: 0 }}>
+                <Switch
+                  checked={isDark}
+                  onChange={(e) =>
+                    setTheme(e.target.checked ? "dark" : "light")
+                  }
+                />
+              </Text>
+            </Card.Body>
+          </Card>
         </Grid>
       </Grid.Container>
-      <Text
-        h1
-        size={60}
-        css={{
-          textGradient: "90deg, $purple600 10%, $blue700 50%",
-        }}
-        weight="bold"
-      >
-        10XBank
-      </Text>
-      <Switch
-        checked={isDark}
-        onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
-      />
     </Container>
   );
 }
